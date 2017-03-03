288fdd57410fb46788548c59df7b5d4d1115c174
galaxy <lixiangchenxy@qq.com>
2017-03-03 22:38:56 +0800

    Some modifies.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-This project is designed to study codon usage for some bacteria genomes. 
-Some useful scripts have been uploaded here.
+This project is designed to study codon usage for some bacteria genomes.
+ 
+Some useful scripts have been uploaded here:
+
+1. calculate
+2. get_RSCU_from_fasta.py --> RSCU
+3. get_CAI_from_fasta.py --> CAI
+4. get_ENC_GC3s_from_fasta.py --> ENC & GC3s
--- get_ENC_GC3s_from_fasta.py
+++ get_ENC_GC3s_from_fasta.py
@@ -33,13 +33,16 @@ def parse_cmdline():
                         help="Output directory")
     parser.add_argument("-t", "--threads", type=int, dest="threads", default=cpu_count(),
                         help="How many threads will be used? [default all]")
+    parser.add_argument("-l", "--logfile", dest="logfile", action="store", default=None,
+                        help="Logfile location")
+    parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", default=False,
+                        help="Give verbose output")
     return parser.parse_args()
 
 
-def run_codonw(each_type, fasta_file, out_file, blk_file):
+def run_codonw(each_type, fasta_file, out_file):
     devnull = open(os.devnull, 'w')
-    cmd = '{0} {1} {2} {3} -noblk -nomenu -nowarn -silent -human -fasta -enc'.format(each_type, fasta_file,
-                                                                                     out_file, blk_file)
+    cmd = '{0} {1} {2} -noblk -nomenu -nowarn -silent -human -fasta -enc'.format(each_type, fasta_file, out_file)
     try:
         subprocess.call(cmd, shell=True, stdout=devnull, stderr=devnull)
     except OSError:
@@ -59,19 +62,15 @@ def multi_process(each_type):
     out_dir = os.path.join(type_dir, 'out')
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-    blk_dir = os.path.join(type_dir, 'blk')
-    if not os.path.exists(blk_dir):
-        os.makedirs(blk_dir)
     for root, dirs, files in os.walk(args.indirname):
         for each_file in files:
             strain_id = os.path.splitext(each_file)[0]
             fasta_file = os.path.join(args.indirname, each_file)
             out_file = os.path.join(out_dir, strain_id + '.out')
-            blk_file = os.path.join(blk_dir, strain_id + '.blk')
-            run_list.append([fasta_file, out_file, blk_file])
+            run_list.append([fasta_file, out_file])
     p = Pool(args.threads)
     for each_item in run_list:
-        p.apply_async(run_codonw, args=(each_type, each_item[0], each_item[1], each_item[2]))
+        p.apply_async(run_codonw, args=(each_type, each_item[0], each_item[1]))
     p.close()
     p.join()
 
@@ -88,12 +87,29 @@ if __name__ == '__main__':
     # Parse command-line
     args = parse_cmdline()
     # Set up logging
-    logger = logging.getLogger('main_process.py: %s' % time.asctime())
+    logger = logging.getLogger('get_ENC_GC3s_from_fasta.py: %s' % time.asctime())
     t0 = time.time()
     logger.setLevel(logging.DEBUG)
     err_handler = logging.StreamHandler(sys.stderr)
     err_formatter = logging.Formatter('%(levelname)s: %(message)s')
     err_handler.setFormatter(err_formatter)
+    # Was a logfile specified? If so, use it
+    if args.logfile is not None:
+        try:
+            log_stream = open(args.logfile, 'w')
+            err_handler_file = logging.StreamHandler(log_stream)
+            err_handler_file.setFormatter(err_formatter)
+            err_handler_file.setLevel(logging.INFO)
+            logger.addHandler(err_handler_file)
+        except IOError:
+            logger.error("Could not open %s for logging", args.logfile)
+            sys.exit(1)
+    # Do we need verbosity?
+    if args.verbose:
+        err_handler.setLevel(logging.INFO)
+    else:
+        err_handler.setLevel(logging.WARNING)
+    logger.addHandler(err_handler)
     if not os.path.exists(args.outdirname):
         os.makedirs(args.outdirname)
     else:
